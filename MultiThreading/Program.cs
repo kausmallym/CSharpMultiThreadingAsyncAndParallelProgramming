6cb50fbee2436b95a4da323f1ed2ba1f6b25a49c
Mehfooz Kausmally <mehfooz.kausmally@sdworx.com>
2023-01-11 16:42:06 +0400

    Execution of the 3 methods using single thread In method 2 sleeps for 10 seconds, Drawback: method 3 has to wait for method 2 to complete to be executed

--- MultiThreading/Program.cs
+++ MultiThreading/Program.cs
@@ -4,3 +4,43 @@ Thread t = Thread.CurrentThread;
 t.Name = "Main Thread";
 Console.WriteLine("Thread Name: {0}", t.Name);
 Console.WriteLine("Current thread Name: {0}", Thread.CurrentThread.Name);
+
+Method1();
+Console.WriteLine("Method 1 execution is completed");
+
+Method2();
+Console.WriteLine("Method 2 execution is completed");
+
+Method3();
+Console.WriteLine("Method 3 execution is completed");
+
+static void Method1()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		Console.WriteLine("Method 1: {0}", i);
+	}
+}
+
+static void Method2()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine("Method 2: {0}", i);
+        if (i == 2)
+        {
+            Console.WriteLine("Method 2 DB operation started");
+            //Sleep for 10 seconds
+            Thread.Sleep(10000);
+            Console.WriteLine("Method 2 DB operation completed");
+        }
+    }
+}
+
+static void Method3()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine("Method 3: {0}", i);
+    }
+}

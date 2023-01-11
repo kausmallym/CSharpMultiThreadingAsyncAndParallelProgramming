73478184e124aad6fbc1e9adf59bf64c5c25fccf
Mehfooz Kausmally <mehfooz.kausmally@sdworx.com>
2023-01-11 12:10:47 +0400

    Add project files.

---
+++ MultiThreading/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+Thread t = Thread.CurrentThread;
+t.Name = "Main Thread";
+Console.WriteLine("Thread Name: {0}", t.Name);
+Console.WriteLine("Current thread Name: {0}", Thread.CurrentThread.Name);

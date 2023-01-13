d443ab20bb65ff1441f7c1ac3ce212c52a5fdb80
Mehfooz Kausmally <mehfooz.kausmally@sdworx.com>
2023-01-13 09:13:39 +0400

    Crerating and attaching threads to the methods

--- MultiThreading/Program.cs
+++ MultiThreading/Program.cs
@@ -1,29 +1,57 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Application uses a single thread
-Thread t = Thread.CurrentThread;
-t.Name = "Main Thread";
-Console.WriteLine("Thread Name: {0}", t.Name);
-Console.WriteLine("Current thread Name: {0}", Thread.CurrentThread.Name);
+//Thread t = Thread.CurrentThread;
+//t.Name = "Main Thread";
+//Console.WriteLine("Thread Name: {0}", t.Name);
+//Console.WriteLine("Current thread Name: {0}", Thread.CurrentThread.Name);
 
-Method1();
-Console.WriteLine("Method 1 execution is completed");
+Console.WriteLine("Main thread started");
 
-Method2();
-Console.WriteLine("Method 2 execution is completed");
+//Creating Threads
+Thread t1 = new Thread(Method1)
+{
+    Name = "Thread 1"
+};
 
-Method3();
-Console.WriteLine("Method 3 execution is completed");
+Thread t2 = new Thread(Method2)
+            {
+                Name = "Thread 2"
+            };
+
+Thread t3 = new Thread(Method3)
+{
+    Name = "Thread 3"
+};
+
+//Executing the methods
+t1.Start();
+t2.Start();
+t3.Start();
+
+Console.WriteLine("Main thread ended");
+
+//Method1();
+//Console.WriteLine("Method 1 execution is completed");
+
+//Method2();
+//Console.WriteLine("Method 2 execution is completed");
+
+//Method3();
+//Console.WriteLine("Method 3 execution is completed");
 
 static void Method1()
 {
+    Console.WriteLine("Method 1 started using thread: {0}", Thread.CurrentThread.Name);
 	for (int i = 0; i < 5; i++)
 	{
 		Console.WriteLine("Method 1: {0}", i);
 	}
+    Console.WriteLine("Method 1 completed using thread: {0}", Thread.CurrentThread.Name);
 }
 
 static void Method2()
 {
+    Console.WriteLine("Method 2 started using thread: {0}", Thread.CurrentThread.Name);
     for (int i = 0; i < 5; i++)
     {
         Console.WriteLine("Method 2: {0}", i);
@@ -35,12 +63,15 @@ static void Method2()
             Console.WriteLine("Method 2 DB operation completed");
         }
     }
+    Console.WriteLine("Method 2 completed using thread: {0}", Thread.CurrentThread.Name);
 }
 
 static void Method3()
 {
+    Console.WriteLine("Method 3 started using thread: {0}", Thread.CurrentThread.Name);
     for (int i = 0; i < 5; i++)
     {
         Console.WriteLine("Method 3: {0}", i);
     }
+    Console.WriteLine("Method 3 completed using thread: {0}", Thread.CurrentThread.Name);
 }

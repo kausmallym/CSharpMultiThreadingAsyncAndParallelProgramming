abe2597d00a53e5047fb24ce3840649f9a9f51b4
Mehfooz Kausmally <mehfooz.kausmally@sdworx.com>
2023-02-22 15:17:04 +0400

    Using lambda expression to define create the delegate

--- MultiThreading/ThreadConstructors.cs
+++ MultiThreading/ThreadConstructors.cs
@@ -23,15 +23,17 @@ namespace MultiThreading
             //ThreadStart obj = delegate () { ShowNumbers(); };
             
             //We can pass the code directly - not really a correct way but we do not have method names in anonymous method
-            ThreadStart obj = delegate () {
-                {
-                    Console.WriteLine($"Current running thread: {Thread.CurrentThread.Name}");
-                    for (int i = 0; i < 5; i++)
-                    {
-                        Console.WriteLine(i);
-                    }
-                }
-            };
+            //ThreadStart obj = delegate () {
+            //    {
+            //        Console.WriteLine($"Current running thread: {Thread.CurrentThread.Name}");
+            //        for (int i = 0; i < 5; i++)
+            //        {
+            //            Console.WriteLine(i);
+            //        }
+            //    }
+            //};
+
+            ThreadStart obj = () => ShowNumbers();
             //Thread t1 = new Thread(ShowNumbers) //CLR is responsible for initialising the ThreadStart delegate
             Thread t1 = new Thread(obj)
             {

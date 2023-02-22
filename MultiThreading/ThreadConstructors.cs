f20d9d988e9c34cff6cc5cc2e33071eed6b01b96
Mehfooz Kausmally <mehfooz.kausmally@sdworx.com>
2023-02-22 17:22:28 +0400

    If we just change the ShowNumbers method parameter to int, the delegate will complain since it takes as parameter an object We use a NumberHelper class in this case and pass the parameter through the constructor while creating an object and then use the ThreadStart delegate

--- MultiThreading/ThreadConstructors.cs
+++ MultiThreading/ThreadConstructors.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection.Metadata;
 
 namespace MultiThreading
 {
@@ -11,7 +7,8 @@ namespace MultiThreading
         public static void RunMethods()
         {
             //UsingThreadStartDelegateConstructor();
-            UsingParameterizedThreadStartDelegate();
+            //UsingParameterizedThreadStartDelegate();
+            UsingParameterizedThreadStartDelegateTypeSafe();
         }
 
         private static void UsingThreadStartDelegateConstructor()
@@ -22,7 +19,7 @@ namespace MultiThreading
             //Thread t1 = new Thread(ShowNumbers)
 
             //ThreadStart obj = delegate () { ShowNumbers(); };
-            
+
             //We can pass the code directly - not really a correct way but we do not have method names in anonymous method
             //ThreadStart obj = delegate () {
             //    {
@@ -47,15 +44,35 @@ namespace MultiThreading
         private static void UsingParameterizedThreadStartDelegate()
         {
             ParameterizedThreadStart obj = new ParameterizedThreadStart(ShowNumbers);
-            Thread t2 = new Thread(obj) { 
-                Name = "Thread 2" 
+            Thread t2 = new Thread(obj)
+            {
+                Name = "Thread 2"
             };
             t2.Start(10);
             //t2.Start();
             //t2.Start("i am a string");
             //issue is if we do not add a parameter when starting the thread, or a value that is not int(a string),
             //no error will be displayed, but a run time error may be generated
+        }
 
+        private static void UsingParameterizedThreadStartDelegateTypeSafe()
+        {
+            int max = 50;
+            NumberHelper nh = new NumberHelper(max);
+
+            //If we try to pass a string value, it shows an error
+            //NumberHelper nh1 = new NumberHelper("");
+
+            //Use ThreadStart delegate object instead of ParameterizedThreadStart delegate since we are already supplying the parameter through the NumberHelper class object
+            //We are not starging the Thread with a parameter
+            ThreadStart obj = new ThreadStart(nh.ShowNumbersTypeSafe);
+
+            Thread t3 = new Thread(obj)
+            {
+                Name = "Thread 3"
+            };
+
+            t3.Start();
         }
 
         private static void ShowNumbers()
@@ -67,10 +84,29 @@ namespace MultiThreading
             }
         }
 
-        private static void ShowNumbers(object maxNum)
+        public static void ShowNumbers(object maxNum)
         {
             Console.WriteLine($"Current running thread: {Thread.CurrentThread.Name}");
             for (int i = 0; i <= Convert.ToInt32(maxNum); i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+    //Creating a helper class
+    public class NumberHelper
+    {
+        private int _Number;
+        public NumberHelper(int num)
+        {
+            _Number = num;
+        }
+
+        public void ShowNumbersTypeSafe()
+        {
+            Console.WriteLine($"Current running thread: {Thread.CurrentThread.Name}");
+            for (int i = 0; i <= _Number; i++)
             {
                 Console.WriteLine(i);
             }
